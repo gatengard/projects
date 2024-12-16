25c73b392d42641d597307ed3543e551973ad64d
Alex <undalexander@gmail.com>
2024-12-16 13:08:16 +0300

    HW

--- 1.7HW.py
+++ 1.7HW.py
@@ -1,14 +1,12 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-my_st = list(students)
-sorted_my_st = sorted(my_st)
-
+sorted_my_st = sorted(students)
 my_dict = {sorted_my_st[0]:round(sum(grades[0])/(len(grades[0])),1),
       sorted_my_st[1]:round(sum(grades[1])/(len(grades[1])),1),
       sorted_my_st[2]:round(sum(grades[2])/(len(grades[2])),1),
       sorted_my_st[3]:round(sum(grades[3])/(len(grades[3])),1),
       sorted_my_st[4]:round(sum(grades[4])/(len(grades[4])),1)}
-print(students)
+print(sorted_my_st)
 name = input('Введите имя ученика: ')
 print(f'Средняя оценка {name} = {my_dict[name]}')
 
--- Agness.Test.py
+++ Agness.Test.py
@@ -3,3 +3,12 @@ a = (input('введите свое имя: '))
 b = (input('введите имя отца: '))
 print(f'если вас зовут {a}, а вашего отца зовут {b}, значит вы - {a + ' ' + b + 'ович'}')
 
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+st = sorted(students)
+gr = []
+for num in grades:
+    i = sum(num) / len(num)
+    gr.append(i)
+my_dict = dict(zip(st,gr))
+print(my_dict)
